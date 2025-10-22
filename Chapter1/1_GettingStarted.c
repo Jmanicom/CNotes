5c3adb8d7f1f2e12d37552b8c0eec094a7abd26a
ymir <joshmanicom@gmail.com>
2025-10-22 16:13:24 -0700

    Initial commit to test if GitHub is Linked

---
+++ Chapter1/1_GettingStarted.c
@@ -0,0 +1,13 @@
+/* The only was to learn a programming language is by writing programs in it.
+The first program to write is a "Hello World" program.
+*/
+
+#include <stdio.h>
+
+int main () 
+{
+    printf("Hello World");
+
+    return 0;
+}
+
