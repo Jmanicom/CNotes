e7f0a28ce69fed3ff11dbabeb3e4fc25cbf80794
ymir <joshmanicom@gmail.com>
2025-10-25 03:03:02 -0700

    Working on Exercise 13

--- Chapter1/Exercises/Exercise13.c
+++ Chapter1/Exercises/Exercise13.c
@@ -5,3 +5,31 @@
 
 #define MAXWORD 10  // Max word length to track
 
+int main()
+{
+    int c, in_blank, wlen;
+    in_blank = wlen = 0;
+
+    int len[MAXWORD + 1] = {0};
+
+    while ((c = getchar()) != EOF) {
+        if ((c != ' ') && (c != '\t') && (c != '\n')) {
+            ++wlen;
+        }
+        else if(wlen > 0) {
+            if (wlen > MAXWORD)
+                wlen = MAXWORD;
+            len[wlen]++;
+            wlen = 0;
+        }
+        }
+    for (int i = 1; i <= MAXWORD; i++) {
+        if (len[i] > 0) {
+            printf("%2d | ", i);
+            for (int j = 0; j < len[i]; j++) {
+                putchar('#');
+            printf(" (%d)\n", len[i]);
+            }
+        }
+    }
+}
